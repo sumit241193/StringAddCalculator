9ad02101bc4d9e9007323de6702f467ecbd046db
Agrawal <Agrawal@DESKTOP-4JOUJ1N>
2020-06-09 01:37:19 +0530

    Commit for 2nd Test Case failure

--- StringCalculator/src/TestStringCalculator.java
+++ StringCalculator/src/TestStringCalculator.java
@@ -1,15 +1,30 @@
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestStringCalculator {
+	
+	private AppStringCalculator calc;
+	
+	@Before
+	public void init()
+	{
+		calc=new AppStringCalculator();
+	}
 	
 	// For Checking Empty String
 	@Test
 	public void emptyString()
 	{
-		AppStringCalculator calc=new AppStringCalculator();
+		
 		assertEquals(calc.add(""), 0);
+	}
+	
+	@Test
+	public void singleValue()
+	{
+		assertEquals(calc.add("1"),1);
 	}
 
 }
