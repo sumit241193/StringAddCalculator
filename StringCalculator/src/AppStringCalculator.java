d37858a5d3724931344d2942023f6e04deb263fb
Agrawal <Agrawal@DESKTOP-4JOUJ1N>
2020-06-09 01:52:57 +0530

    Commit 3rd case pass i.e 2 numbers using comma

--- StringCalculator/src/AppStringCalculator.java
+++ StringCalculator/src/AppStringCalculator.java
@@ -2,20 +2,24 @@
 public class AppStringCalculator {
 
 	public int add(String number) {
-		if(isEmpty(number))
+
+		String[] values = number.split(",");
+
+		if (isEmpty(number))
 			return 0;
-		
-		return	strToInt(number);
+
+		if (number.length() == 1)
+			return strToInt(number);
+		else
+
+			return Integer.parseInt(values[0]) + Integer.parseInt(values[1]);
+
 	}
-
-
 
 	private boolean isEmpty(String number) {
 		return number.isEmpty();
 	}
 
-	
-	
 	private int strToInt(String number) {
 		return Integer.parseInt(number);
 	}
